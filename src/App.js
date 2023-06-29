11983066b852a3aa5e2d2a8dfd01afc864702c56
Pushpendra Sharma <pushpendra.sharma@loblaw.ca>
2023-06-29 21:44:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import DataTable from "react-data-table-component";
+import { res } from "./res";
+import Card from "@mui/material/Card";
+import { CardContent, Typography, Grid } from "@mui/material";
 
 function App() {
+  const customStyles = {
+    rows: {
+      style: {
+        minHeight: "108px", // override the row height
+        width: "100%",
+        fontSize: "16px",
+      },
+    },
+    headCells: {
+      style: {
+        backgroundColor: "#F0F0F0",
+        borderBottom: "3px solid #666B73",
+        fontSize: "16px",
+        fontWeight: "bolder", // override the cell padding for head cells
+      },
+    },
+    cells: {
+      style: {
+        width: "30%",
+        paddingLeft: "16px", // override the cell padding for data cells
+        paddingRight: "8px",
+      },
+    },
+  };
+
+  const Columns = [
+    {
+      name: "Postion",
+      selector: (row) => row.position,
+    },
+    {
+      name: "Job Title",
+      selector: (row) => row.title,
+    },
+    {
+      name: "Skills",
+      selector: (row) => row.skills,
+    },
+    {
+      name: "Job Description",
+      selector: (row) => row.jobDescription,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Card>
+        <CardContent sx={{ padding: "0px" }}>
+          <Typography variant="h5" sx={{ mb: 3 }}>
+            View Applicants
+          </Typography>
+          <Grid width="100%">
+            <DataTable
+              persistTableHead
+              columns={Columns}
+              noDataComponent={
+                <Typography mt={2}>There are no past orders</Typography>
+              }
+              data={res}
+              customStyles={customStyles}
+              highlightOnHover
+            />
+          </Grid>
+        </CardContent>
+      </Card>
+    </>
   );
 }
 
---
+++ src/res.js
@@ -0,0 +1,86 @@
+export const res = [
+    {
+      id: 1,
+      position: "Software Engineer",
+      title: "Backend Developer",
+      skills: ["Python", "Javascript", "Redis"],
+      jobDescription: "We are looking for a creative, skilled UI developer to design websites with functionality and aesthetics in mind. The UI developer will work with back-end developers and web developers to ensure that the website is optimized for several devices and presented in an attractive way."
+    },
+    {
+      id: 2,
+      position: "Software Engineer",
+      title: "Frontend Developer",
+      skills: ['next js', 'react js', 'angular js'],
+      jobDescription: "We are looking for a creative, skilled UI developer to design websites with functionality and aesthetics in mind. The UI developer will work with back-end developers and web developers to ensure that the website is optimized for several devices and presented in an attractive way."
+    },
+    {
+      id: 3,
+      position: "Software Engineer",
+      title: "Backend Developer",
+      skills: ["Python", "Javascript", "Redis"],
+      jobDescription: "We are looking for a creative, skilled UI developer to design websites with functionality and aesthetics in mind. The UI developer will work with back-end developers and web developers to ensure that the website is optimized for several devices and presented in an attractive way."
+    },
+    {
+      id: 4,
+      position: "Software Engineer",
+      title: "Frontend Developer",
+      skills: ['next js', 'react js', 'angular js'],
+      jobDescription: "We are looking for a creative, skilled UI developer to design websites with functionality and aesthetics in mind. The UI developer will work with back-end developers and web developers to ensure that the website is optimized for several devices and presented in an attractive way."
+    },
+    {
+      id: 5,
+      position: "Software Engineer",
+      title: "Backend Developer",
+      skills: ["Python", "Javascript", "Redis"],
+      jobDescription: "We are looking for a creative, skilled UI developer to design websites with functionality and aesthetics in mind. The UI developer will work with back-end developers and web developers to ensure that the website is optimized for several devices and presented in an attractive way."
+    },
+    {
+      id: 6,
+      position: "Software Engineer",
+      title: "Frontend Developer",
+      skills: ['next js', 'react js', 'angular js'],
+      jobDescription: "We are looking for a creative, skilled UI developer to design websites with functionality and aesthetics in mind. The UI developer will work with back-end developers and web developers to ensure that the website is optimized for several devices and presented in an attractive way."
+    },
+    {
+      id: 7,
+      position: "Software Engineer",
+      title: "Backend Developer",
+      skills: ["Python", "Javascript", "Redis"],
+      jobDescription: "We are looking for a creative, skilled UI developer to design websites with functionality and aesthetics in mind. The UI developer will work with back-end developers and web developers to ensure that the website is optimized for several devices and presented in an attractive way."
+    },
+    {
+      id: 8,
+      position: "Software Engineer",
+      title: "Frontend Developer",
+      skills: ['next js', 'react js', 'angular js'],
+      jobDescription: "We are looking for a creative, skilled UI developer to design websites with functionality and aesthetics in mind. The UI developer will work with back-end developers and web developers to ensure that the website is optimized for several devices and presented in an attractive way."
+    },
+    {
+      id: 9,
+      position: "Software Engineer",
+      title: "Backend Developer",
+      skills: ["Python", "Javascript", "Redis"],
+      jobDescription: "We are looking for a creative, skilled UI developer to design websites with functionality and aesthetics in mind. The UI developer will work with back-end developers and web developers to ensure that the website is optimized for several devices and presented in an attractive way."
+    },
+    {
+      id: 10,
+      position: "Software Engineer",
+      title: "Frontend Developer",
+      skills: ['next js', 'react js', 'angular js'],
+      jobDescription: "We are looking for a creative, skilled UI developer to design websites with functionality and aesthetics in mind. The UI developer will work with back-end developers and web developers to ensure that the website is optimized for several devices and presented in an attractive way."
+    },
+    {
+      id: 11,
+      position: "Software Engineer",
+      title: "Backend Developer",
+      skills: ["Python", "Javascript", "Redis"],
+      jobDescription: "We are looking for a creative, skilled UI developer to design websites with functionality and aesthetics in mind. The UI developer will work with back-end developers and web developers to ensure that the website is optimized for several devices and presented in an attractive way."
+    },
+    {
+      id: 12,
+      position: "Software Engineer",
+      title: "Frontend Developer",
+      skills: ['next js', 'react js', 'angular js'],
+      jobDescription: "We are looking for a creative, skilled UI developer to design websites with functionality and aesthetics in mind. The UI developer will work with back-end developers and web developers to ensure that the website is optimized for several devices and presented in an attractive way."
+    },
+  ];
